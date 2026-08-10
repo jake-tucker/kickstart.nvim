9fc15d26ccbf365ecdabd3865637ef56cc61e6c5
Jake Tucker <8502487+jake-tucker@users.noreply.github.com>
2026-08-10 21:04:37 +0800

    Enable TS and C# language servers

--- init.lua
+++ init.lua
@@ -727,8 +727,8 @@ do
     --    https://github.com/pmizio/typescript-tools.nvim
     --
     -- But for many setups, the LSP (`ts_ls`) will work just fine
-    -- ts_ls = {},
-
+    ts_ls = {},
+    roslyn_ls = {},
     stylua = {}, -- Used to format Lua code
 
     -- Special Lua Config, as recommended by neovim help docs
